ca3b72bf7b41fe54c42133cb9af0d63ce632d71c
Patrick <Isola>
2020-10-31 22:06:12 +0100

    lato source code pro

--- 03-structures-controle/ifelse.js
+++ 03-structures-controle/ifelse.js
@@ -2,9 +2,9 @@
 
 let x = 0.5;
 
-if(x > 1){// si le test est vrai j'exécute le bloc qui suit
+if (x > 1) {// si le test est vrai j'exécute le bloc qui suit
     alert('x contient une valeur supérieure à 1');
-}else{//sinon j'exécute celui
+} else {//sinon j'exécute celui
     alert('x contient une valeur inférieure à 1');
 }
 
