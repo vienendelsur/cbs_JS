310271c093914809a5fe5c4b77398bd21014a401
Patrick <Isola>
2020-11-08 10:44:06 +0100

    suite

--- 04-fonctions/01-fonctions.js
+++ 04-fonctions/01-fonctions.js
@@ -5,11 +5,7 @@
 document.getElementById('p1').innerHTML = Math.random();
 
 //replace()
-
 let discours1 = 'Bonjour, je suis Donald Trump, et j\'ai gagné !';
-
-console.log(discours1);
-
+    console.log(discours1);
 let discours2 = discours1.replace('Donald Trump', 'Joe Biden');
-
 document.getElementById('p2').innerHTML = discours2;
