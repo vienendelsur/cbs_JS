2085825f02c60f73f857885d878141da2e38014d
Patrick Isola <vienendelsur@users.noreply.github.com>
2020-11-16 09:58:01 +0100

    01-bonjour

---
+++ 00-exos/01-bonjour.js
@@ -0,0 +1,13 @@
+// scripts pour la page 01-bonjour.html
+
+//var prenom = prompt("Entrez votre prénom");
+// alert("Bonjour, " + prenom);
+
+// var saisie = prompt("Entrez un nombre");// nb est de type chaine
+// var nb = Number(saisie); // nb est de type nombre
+
+// var nb1 = Number(prompt("Entrez un nombre : ")); // nb1 est de type nombre
+
+var prenom = prompt("Entrez votre prénom : ");
+var nom = prompt("Entrez votre nom : ");
+alert("Bonjour, " + prenom + " " + nom);
