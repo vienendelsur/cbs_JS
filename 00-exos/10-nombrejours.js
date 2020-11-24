27ac950a8257e3538e63160115e9b67fbfbfb29c
Patrick Isola <vienendelsur@users.noreply.github.com>
2020-11-24 10:40:01 +0100

    nombrejours

--- 00-exos/10-nombrejours.js
+++ 00-exos/10-nombrejours.js
@@ -12,11 +12,20 @@ if (isNaN(mois)) {//si ce n'est pas un number
         document.getElementById('p1').innerHTML = 'Votre choix n\'est pas un chiffre.';//fait ça 
     } else { /* sinon c'est un nombre */
             if (mois === 2) {//un nombre égal à 2 etc...
-                document.getElementById('p1').innerHTML = 'Le mois numéro ' + mois + ' de l\'année fait 28 jours.';
+                // document.getElementById('p1').innerHTML = 'Le mois numéro ' + mois + ' de l\'année fait 28 jours.';
+
+                document.getElementById('p1').innerHTML = 'Le mois numéro ' + mois + ', ' + moisNom[mois - 1] + ' fait 28 jours.'
+
             } else if ((mois === 4) || (mois === 6) || (mois === 9) || (mois === 11)) {// OU à 4 OU à 6 etc...
-                document.getElementById('p1').innerHTML = 'Le mois numéro ' + mois + ' de l\'année fait 30 jours.';
+                // document.getElementById('p1').innerHTML = 'Le mois numéro ' + mois + ' de l\'année fait 30 jours.';
+
+                document.getElementById('p1').innerHTML = 'Le mois numéro ' + mois + ', ' + moisNom[mois - 1] + ' fait 30 jours.'
+
             } else if ((mois === 1) || (mois === 3) || (mois === 5) || (mois === 7) || (mois === 8) || (mois === 10) || (mois === 12)) {
-                document.getElementById('p1').innerHTML = 'Le mois numéro ' + mois + ' de l\'année fait 31 jours.';
+                // document.getElementById('p1').innerHTML = 'Le mois numéro ' + mois + ' de l\'année fait 31 jours.';
+
+                document.getElementById('p1').innerHTML = 'Le mois numéro ' + mois + ', ' + moisNom[mois - 1] + ' fait 31 jours.'
+
             } else {
                 document.getElementById('p1').innerHTML = 'Votre choix de numéro, le ' + mois + ' n\est pas dans le calendrier ! ';
         }
