1ee0d5c4f33e0c271187f263876ba8576322c569
Patrick Isola <isola.patrick@gmail.com>
2020-11-27 08:56:45 +0100

    sélecteurs

---
+++ 06-selecteurs/selecteurs.js
@@ -0,0 +1,9 @@
+//page pour les sélecteurs
+
+// avec cette évènement window.onload toute la page est chargée et cela me permet de mettre le script dans <head>, sans "async"
+//onload = au chargement de la page
+// window.onload = function() {
+//     // sélection par un id
+//     var select1 = titreNiveau1;
+//     console.log(select1);  
+// }
