e989392a9942a1d1913d7d652cfe12f278b0dffe
Patrick Isola <isola.patrick@gmail.com>
2020-12-03 14:09:17 +0100

    exo et cours

---
+++ 00-exos/17-triangle.js
@@ -0,0 +1,22 @@
+// triangle dans la console avec deux boucles 
+
+console.log('Le triangle avec une boucle for');
+
+let diese = '';
+
+// console.log(typeof diese, diese);
+
+for (let i = 1;  i < 10; i++) {
+    diese += '#';
+    console.log(diese);
+}
+
+console.log('Le triangle avec une boucle for correction Baroude');
+
+for (i = 0; i < 10; i++) {
+    console.log('#'.repeat(i));
+}
+
+// avec une boucle while 
+
+let diese2 = '';
