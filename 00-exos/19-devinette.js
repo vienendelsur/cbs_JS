c4f7aafb8f5e7b71e672f76648d3bb5d21104828
Patrick Isola <isola.patrick@gmail.com>
2020-12-10 12:29:01 +0100

    formulaire

--- 00-exos/19-devinette.js
+++ 00-exos/19-devinette.js
@@ -2,17 +2,17 @@
 // confirm('Bienvenue dans le jeu !');
 
 // // on doit trouver un nombre au hasard que l'utilisateur doit deviner en 3 étapes
-// // 1- le nombre
-// let nombreAuHasard = Math.random();// nombre décimal entre 0 et 1
-// console.log(typeof nombreAuHasard, nombreAuHasard);
+// 1- le nombre
+let nombreAuHasard = Math.random();// nombre décimal entre 0 et 1
+console.log(typeof nombreAuHasard, nombreAuHasard);
 
-// // 2- on multiplie le nombre trouvé par 100
-// let nombreX100 = nombreAuHasard * 100;
-// console.log(nombreX100);
+// 2- on multiplie le nombre trouvé par 100
+let nombreX100 = nombreAuHasard * 100;
+console.log(nombreX100);
 
-// // 3- on arrondi le nombre
-// let nombreArrondi = Math.floor(nombreX100);//Math.floor arrondi un nombre décimal au nombre inférieur
-// console.log(nombreArrondi);
+// 3- on arrondi le nombre
+let nombreArrondi = Math.floor(nombreX100);//Math.floor arrondi un nombre décimal au nombre inférieur
+console.log(nombreArrondi);
 
 // 2 et 3 autre solution pour arrondir le nombre
 // nombreX100 = parseInt(nombreAuHasard * 100);
